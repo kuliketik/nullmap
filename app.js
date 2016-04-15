7be173933076ffead36065fcad8c9d066cfb1359
vickydasta <vickydasta@gmail.com>
2016-04-15 11:44:21 +0700

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var express = require("express"), 
-	app = express();
 
-app.use('/', express.static('static'));
-app.listen(8080);
+var express = require("express"); 
+var app = express();
+
+app.set('port' (process.env.PORT || 5000);
+app.set('views', __dirname+'/static'));
+
+app.get('/', funtion(req, res){
+	res.render('static/index.html')
+});
+
+app.listen(app.get('port'), function(){
+	console.log('node running on port ', app.get('port'));
+});
